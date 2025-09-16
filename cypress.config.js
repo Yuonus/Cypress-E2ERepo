df1e1fe7948427237de05577b8a5ba2d5847b3a3
Yuonus <syuonus@ndc.nasa.gov>
2025-09-16 09:14:51 -0400

    New Commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,13 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: 'cypress/reports/html',
-    overwrite: false,
-    html: true,
-    json: true,
-    cdn: true, // ✅ This makes the HTML report use CDN-hosted assets
-  },
+  
   e2e: {
     baseUrl: 'https://naveenautomationlabs.com/opencart/index.php?route=account/login',
     setupNodeEvents(on, config) {
